b5992d876ce0ee97a05edbb336fca105adb5620a
Tory Rachel Van Tassel <countrygirlr09@hotmail.com>
2021-09-08 20:58:13 -0600

    completed mvp

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // require your server and launch it
+const server = require('./api/server');
+const express = require('express');
+
+const PORT = process.env.PORT || 3003
+const app = express();
+
+app.get('/api', (req, res) => {
+  res.json({
+    message: 'Hello from the server!'
+  });
+});
+
+server.listen(4000, () => {
+    console.log('\n* Server Running on http://localhost:4000 *\n');
+  });
+
+
